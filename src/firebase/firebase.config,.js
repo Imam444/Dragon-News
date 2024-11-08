16f10bc2ba769b98d342d5b2ceb02b9cab6531c0
Imam444 <mdimamhossain565528@gmail.com>
2024-11-09 00:38:22 +0600

    React projrct Folder Structure and Router setup

---
+++ src/firebase/firebase.config,.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBky63zs94f7rBDzZN5J-Zdbk_5wBZJ_N8",
+  authDomain: "the-dragon-news-3687e.firebaseapp.com",
+  projectId: "the-dragon-news-3687e",
+  storageBucket: "the-dragon-news-3687e.firebasestorage.app",
+  messagingSenderId: "880278472616",
+  appId: "1:880278472616:web:d9240dc25e098169853f9d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export defaoutlt app
