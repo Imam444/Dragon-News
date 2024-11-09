e0e48a997fd36a48bcc2425b09fe0e24250a1a46
Imam444 <mdimamhossain565528@gmail.com>
2024-11-10 01:55:25 +0600

    HomePage Layout and date time formating using moment js

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,24 @@
 import Header from "../Shared/Header/Header";
 import Navbar from "../Shared/Navbar/Navbar";
+import RightSideNav from "../Shared/RightSideNav/RightSideNav";
+import LeftSideNav from './../Shared/LeftSideNav/LeftSideNav';
 
 const Home = () => {
     return (
         <div>
             <Header></Header>
             <Navbar></Navbar>
-            <h3 className="text-3xl font-poppins">This Home page</h3>
+            <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
+                <div className="border">
+                    <LeftSideNav></LeftSideNav>
+                </div>
+                <div className="md:col-span-2 border">
+                    <h3 className="text-2xl">News Coming Soon........</h3>
+                </div>
+                <div className="border">
+                    <RightSideNav></RightSideNav>
+                </div>
+            </div>
         </div>
     );
 };
--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,11 +1,13 @@
-
-
+import moment from "moment";
+import logo from "../../../assets/logo.png";
 const Header = () => {
-    return (
-        <div>
-            <h2 className="text 3xl">Header</h2>
-        </div>
-    );
+  return (
+    <div>
+      <img src={logo} alt="" />
+      <p>Journalism Without Fear or Favour</p>
+      <p className="xl">{moment().format("MMMM D YYYY, h:mm:ss a")}</p>
+    </div>
+  );
 };
 
 export default Header;
